4faa8006c8d76743c94178bc9ae2f647f1eac643
Bekzod Abdulkhaev <abdulkhaev96@gmail.com>
2024-04-15 23:00:58 +0900

    initial

--- src/components/pages/home.jsx
+++ src/components/pages/home.jsx
@@ -1,5 +1,19 @@
+import Button from "@mui/joy/Button";
+import MotorTable from "../tables/motorTable";
+import { useState } from "react";
+
 const HomeComponent = () => {
-  return <div>HomeComponent</div>;
+  const [variant, setVariant] = useState("solid");
+  return (
+    <div style={{ padding: "50px" }}>
+      <div style={{ display: "flex", justifyContent: "end" }}>
+        <Button size="md" variant={variant} color="success">
+          Add New
+        </Button>
+      </div>
+      <MotorTable />
+    </div>
+  );
 };
 
 export default HomeComponent;
